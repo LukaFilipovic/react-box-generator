0157398fac58e0e55e334259462cabe14cd21b18
Luka Bulatovic <luka.bulatovic@ibis-instruments.com>
2019-08-21 20:35:17 +0200

    Added drag handler

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import './Box.scss';
 export default class Box extends Component {
+    dragEnd = (e) => {
+        // this.props.dragFunction(this.props.id)
+        // e.dataTransfer.setData("text/html", e.parentNode) 
+        // e.dataTransfer.setDragImage(e.parentNode, 20, 20)
+    }
     render() {
         return (
-            <div className='Box'
+            <div className='Box' draggable="true" onDragStart={e =>this.props.dragStart(e,  this.props.index) } onDragEnd={() => this.props.dragEnd } onDragOver={() => this.props.dragOver(this.props.index)}
                 style={{
                     backgroundColor: `${this.props.color}`
                 }}>
